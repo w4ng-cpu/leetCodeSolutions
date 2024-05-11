08e59691e1291749f968087a0bf855b8e05228ae
w4ng-cpu <allankenwang@gmail.com>
2024-05-11 21:13:16 +0100

    27-removeElement space optimised and cleaner

--- src/27-removeElement.js
+++ src/27-removeElement.js
@@ -4,21 +4,10 @@
  * @return {number}
  */
 var removeElement = function (nums, val) {
-    let endPointer = nums.length - 1;
     let count = 0;
-    for (let i = 0; i <= endPointer; i++) {
-        if (nums[i] === val) {
-            for (; endPointer >= i; endPointer--) {
-                if (nums[endPointer] != val) {
-                    nums[i] = nums[endPointer];
-                    endPointer--
-                    count++;
-                    break;
-                }
-            }
-        }
-        else {
-            count++;
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] !== val) {
+            nums[count++] = nums[i];
         }
     }
 

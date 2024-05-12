8f539df342b4ccd58dc53fdd579f9a02428c1a82
w4ng-cpu <allankenwang@gmail.com>
2024-05-12 16:03:48 +0100

    optimised removeDuplicatesFromSortedArray2

--- src/80-removeDuplicatesFromSortedArray2.js
+++ src/80-removeDuplicatesFromSortedArray2.js
@@ -3,19 +3,11 @@
  * @return {number}
  */
 var removeDuplicates = function (nums) {
-    let appearedTwice = false;
     let count = 1;
 
     for (let i = 1; i < nums.length; i++) {
-        if (nums[i] === nums[i - 1]) {
-            if (!appearedTwice) {
-                nums[count++] = nums[i];
-                appearedTwice = true;
-            }
-        }
-        else {
-            nums[count++] = nums[i]
-            appearedTwice = false;
+        if (count == 1 || nums[i] !== nums[count - 2]) {
+            nums[count++] = nums[i]; 
         }
     }
     return count;
